984140956c9d9a8fcd16a87f3e8b9d7ad2c33a5c
Sunny Gaonkar <sunnygnkr93@gmail.com>
2022-12-11 09:49:18 +0530

    Some more chages made in 2nd changes

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,5 +2,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test class is printing");
+        System.out.println("made some changes");
+
+        int num =  4;
+        int sqr = (int) Math.pow(4,2);
+        System.out.println(sqr);
     }
 }
